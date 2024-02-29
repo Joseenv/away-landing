1527855c546ac9325fb8d419f2ffe2556e6993be
Joseenv <jose229901@gmail.com>
2024-02-29 00:53:00 -0500

    [ Jose Navarro - feat ] : Adding SEO Data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,19 @@
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en-US',
+      },
+      title: 'Best selling travel products | Away: Built for modern travel',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Best selling travel products | Away: Built for modern travel' },
+      ],
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-swiper',
